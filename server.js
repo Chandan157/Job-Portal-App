58479eaea6f22ba1e9db93ad8b272a7f2313f056
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-04-15 16:52:14 +0530

    setup completed

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-import colors from "colors";  
+import colors from "colors";
+import connectDB from "./config/db.js";
+import testRoutes from "./routes/testRoutes.js"
+import cors from "cors"
+import morgan from "morgan";
 
 dotenv.config();
 
+//mongodb connection
+connectDB();
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Job Portal</h1>");
-});
+app.use(express.json());
+app.use(cors())
+app.use(morgan("dev"));
+
+//Routes
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Welcome to Job Portal</h1>");
+// });
+
+app.use('/api/v1/test',testRoutes)
 
 const PORT = process.env.PORT || 8080;
 
